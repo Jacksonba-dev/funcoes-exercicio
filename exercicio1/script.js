2460815725a9b1acaa1bbf2ba51bc15ac0d77903
Seu nome <jacksonsouza.consultor@gmail.com>
2022-10-25 15:26:00 -0300

    exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+//Exercicio1 
+
+//a
+
+function imprimeNome (seuNome) {
+    console.log(`Olá, ${seuNome}!`)
+}
+
+imprimeNome('Jackson')
+
+//b
+
+function tabuada (numero) {
+    console.log(`${numero * 1}`)
+    console.log(`${numero * 2}`)
+    console.log(`${numero * 3}`)
+    console.log(`${numero * 4}`)
+    console.log(`${numero * 5}`)
+    console.log(`${numero * 6}`)
+    console.log(`${numero * 7}`)
+    console.log(`${numero * 8}`)
+    console.log(`${numero * 9}`)
+    console.log(`${numero * 10}`)
+}
+
+tabuada(3)
